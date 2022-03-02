610bd6b18bcb1e13eea3310fe889deb60244c905
Papa Ba GAYE <83582380+PapaBaGAYE@users.noreply.github.com>
2022-03-02 16:32:39 +0000

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,26 @@
+CREATE TABLE `SRC_Vente` ( 
+  `id` INT NOT NULL,
+  `CodeClient` VARCHAR(20) NOT NULL,
+  `CodeProduit` VARCHAR(20) NOT NULL, 
+  `DateVente` DATE NULL,
+  `PrixUnitaire` INT NULL,
+  `Quantité` INT NULL,
+PRIMARY KEY (`id`));
+
+CREATE TABLE `SAS_Vente` ( 
+    `id` INT NOT NULL,
+  `CodeClient` VARCHAR(20) NOT NULL,
+  `CodeProduit` VARCHAR(20) NOT NULL, 
+  `DateVente` DATE NULL,
+  `PrixUnitaire` INT NULL,
+  `Quantité` INT NULL,
+PRIMARY KEY (`id`));
+
+CREATE TABLE `REJET_Vente` ( 
+    `id` INT NOT NULL,
+  `CodeClient` VARCHAR(20) NOT NULL,
+  `CodeProduit` VARCHAR(20) NOT NULL, 
+  `DateVente` DATE NULL,
+  `PrixUnitaire` INT NULL,
+  `Quantité` INT NULL,
+PRIMARY KEY (`id`));
